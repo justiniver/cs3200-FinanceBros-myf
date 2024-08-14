67fa54ce63c4a56ebff42e71004937dad850bfc3
Bryan Guen <guen.br@northeastern.edu>
2024-08-14 16:50:12 -0400

    MAking changed to front end connection

--- app/src/pages/00_Reg_User_Home.py
+++ app/src/pages/00_Reg_User_Home.py
@@ -14,10 +14,10 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('View World Bank Data Visualization', 
+if st.button('View My Portfolio', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/01_World_Bank_Viz.py')
+  st.switch_page('pages/01_Portfolio_Info_Viz.py')
 
 if st.button('View World Map Demo', 
              type='primary',
