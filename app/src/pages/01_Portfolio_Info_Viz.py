f723e5fdf5216b834a4a9785f96b067a6039a72f
Bryan Guen <guen.br@northeastern.edu>
2024-08-14 18:15:34 -0400

    implemented frontend to backend for p1

--- app/src/pages/01_Portfolio_Info_Viz.py
+++ app/src/pages/01_Portfolio_Info_Viz.py
@@ -13,16 +13,19 @@ st.write(f"### Hi, {st.session_state['first_name']}.")
 
 data = {} 
 try:
-  data = requests.get('http://api:4000/d/portfolios').json()
+  data = requests.get('http://api:4000/u/myportfolios/9379').json()
 except:
   st.write("**Important**: Could not connect to sample api, so using dummy data.")
   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
 
 st.dataframe(data)
 
+
+st.write(f"### Your Positions:")
+
 data = {} 
 try:
-  data = requests.get('http://api:4000/u/portfolioStocks').json()
+  data = requests.get('http://api:4000/u/portfolios_stock/9379').json()
 except:
   st.write("**Important**: Could not connect to sample api, so using dummy data.")
   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
