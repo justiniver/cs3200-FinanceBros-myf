40f1f2994a708c7b0ac2bc3d94df292388f6bc90
Justin Iverson <justiniver@icloud.com>
2024-08-14 21:40:17 -0400

    app name typo fix

--- app/src/Home.py
+++ app/src/Home.py
@@ -34,7 +34,7 @@ SideBarLinks(show_home=True)
 
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('FinacneBros App')
+st.title('FinanceBros App')
 st.write('\n\n')
 st.write('### Greetings! As which user would you like to log in?')
 
