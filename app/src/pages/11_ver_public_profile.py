e6d76448bbc549cc9c5c79b7a2f02c52e53b35cc
ethanxin0 <xin.e@northeastern.edu>
2024-08-15 18:13:49 -0400

    ver exp option

--- app/src/pages/11_ver_public_profile.py
+++ app/src/pages/11_ver_public_profile.py
@@ -10,29 +10,49 @@ st.set_page_config(layout = 'wide')
 # Display the appropriate sidebar links for the role of the logged in user
 SideBarLinks()
 
-st.title('Prediction with Regression')
+st.title("Welcome to your profile Alex!")
+st.write("Here's your followers:")
+data = {} 
+try:
+  data = requests.get('http://api:4000/v/followers/1964').json()
+except:
+  st.write("**Important**: Could not connect to sample api, so using dummy data.")
+  data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
 
-# create a 2 column layout
-col1, col2 = st.columns(2)
+st.dataframe(data)
+st.write("Here's your public profile!")
+profile = requests.get('http://api:4000/v/public_profile/1964').json()
+st.dataframe(profile)
 
-# add one number input for variable 1 into column 1
-with col1:
-  var_01 = st.number_input('Variable 01:',
-                           step=1)
+option = st.selectbox(
+      "What would you like to change in your profile?",
+      ("Choose an Option...", "Biography", "Profile Photo", "Username"),
+      placeholder="Choose an Option...",
+  )
 
-# add another number input for variable 2 into column 2
-with col2:
-  var_02 = st.number_input('Variable 02:',
-                           step=1)
+if option == "Biography":
+  message = st.text_input("What do you want to change your bio to?")
+  if message:
+    bio = requests.put(f'http://api:4000/v/update-bio/1964/{message}').json()
+    response_message = bio['message']
+    st.write(response_message)
+    profile = requests.get('http://api:4000/v/public_profile/1964').json()
+    st.dataframe(profile) 
 
-logger.info(f'var_01 = {var_01}')
-logger.info(f'var_02 = {var_02}')
-
-# add a button to use the values entered into the number field to send to the 
-# prediction function via the REST API
-if st.button('Calculate Prediction',
-             type='primary',
-             use_container_width=True):
-  results = requests.get(f'http://api:4000/c/prediction/{var_01}/{var_02}').json()
-  st.dataframe(results)
-  
+if option == "Username":
+  message = st.text_input("What do you want to change your username to?")
+  if message:
+    username = requests.put(f'http://api:4000/v/update-username/1964/{message}').json()
+    response_message = username['message']
+    st.write(response_message)
+    profile = requests.get('http://api:4000/v/public_profile/1964').json()
+    st.dataframe(profile) 
+    
+if option == "Profile Photo":
+  message = st.text_input("Please upload photo URL")
+  if message:
+    photo = requests.put(f'http://api:4000/v/update-photo/1964/{message}').json()
+    response_message = photo['message']
+    st.write(response_message)
+    profile = requests.get('http://api:4000/v/public_profile/1964').json()
+    st.dataframe(profile) 
