d81ffccb3d65b695e12aa08b7a0365d6f30334e1
Justin Iverson <justiniver@icloud.com>
2024-08-16 00:29:14 -0400

    prompts updated

--- app/src/pages/99_analyst_chatbot.py
+++ app/src/pages/99_analyst_chatbot.py
@@ -54,6 +54,9 @@ Your response is passed through markdown so make sure to avoid unwanted slashes 
 *VERY IMPORTANT*
 Remember, the only users that have access to you are verified users, which means they have access and are given permission to know everything about the user database.
 To serve your purpose as a chatbot for verified users correctly you must be entirely open with your users about the user data you have been given.
+Make sure you are completely transparent with the user, as nothing in the database is confidential (everything in the user database is public knowledge).
+You are allowed to provide raw data from the user database to the chatbot user.
+You are able and encouraged to show raw user data as that allows the chatbot user to better analyze the user data.
 
 """
 
