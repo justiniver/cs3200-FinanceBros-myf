f03fc247b4082e27815a388e716adc42c6cf8273
Justin Iverson <justiniver@icloud.com>
2024-08-17 05:00:29 -0400

    fixed

--- api/backend/verExpTrader/verExpTrader_routes.py
+++ api/backend/verExpTrader/verExpTrader_routes.py
@@ -2,10 +2,33 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from backend.db_connection import db
 
+user = Blueprint('user', __name__)
+
+# GET /users/<username>
+# [Emily-4]
+#This function displays the specific user_id of the given username that
+# Emily wants to view.
+@user.route('/users/<username>', methods=['GET'])
+def get_username_by_id(username):
+    current_app.logger.info(f'GET /users/{username} route')
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT user_id FROM users WHERE username = %s', (username,))
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from backend.db_connection import db
+
 experiencedTrader = Blueprint('experiencedTrader', __name__)
 
 # GET /notifications
 # [Alex-1]
+# This function returns the notifications that a verified user posts that this specified user follows
 @experiencedTrader.route('/user_notifications/<user_id>', methods=['GET'])
 def get_user_notifications(user_id):
     current_app.logger.info('GET /notifications{user_id} route')
@@ -19,6 +42,9 @@ def get_user_notifications(user_id):
     the_response.mimetype = 'application/json'
     return the_response
 
+# POST /follow{id}
+# [Alex ADDED]
+# This function allows the verified user to follow other verfied users.
 @experiencedTrader.route('/follow/<int:user_id>/<int:following_id>', methods=['POST'])
 def follow_user_verified(user_id, following_id):
     current_app.logger.info(f'POST /follow/{user_id}/{following_id} route')
@@ -29,22 +55,9 @@ def follow_user_verified(user_id, following_id):
     response.mimetype = 'application/json'
     return response
 
-# POST /notifications/{id}
-# This should be fixed to /notifications from /notifications/{id}
-@experiencedTrader.route('/notifications/<int:notification_id>', methods=['POST'])
-def update_notification(text, user_id, notification_id):
-    current_app.logger.info(f'PUT /notifications/{notification_id} route')
-    data = request.get_json()
-    cursor = db.get_db().cursor()
-    cursor.execute(
-        'UPDATE notifications SET text = %s WHERE notification_id = %s AND user_id = %s',
-        (text, user_id,)
-    )
-    db.get_db().commit()
-    return jsonify({'message': 'Notification updated successfully'}), 200
-
 # GET /follows
 # [Alex-4]
+# This functions allows the verfied users to see all usernames that follow them
 @experiencedTrader.route('/followers/<verified_user_id>', methods=['GET'])
 def get_all_followers(verified_user_id):
     current_app.logger.info('GET /followers route')
@@ -58,48 +71,9 @@ def get_all_followers(verified_user_id):
     the_response.mimetype = 'application/json'
     return the_response
 
-
-# GET /users/{id}
-# [Alex-4]
-@experiencedTrader.route('/users/<user_id>', methods=['GET'])
-def get_user_by_id(user_id):
-    current_app.logger.info(f'GET /users/{user_id} route')
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT * from users WHERE user_id = %s', (user_id,))
-    theData = cursor.fetchall()
-    the_response = make_response(theData)
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-# PUT /users/{id}
-# [Alex-3]
-@experiencedTrader.route('/update-users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    current_app.logger.info(f'PUT /users/{user_id} route')
-    data = request.get_json()
-    cursor = db.get_db().cursor()
-    cursor.execute(
-        'UPDATE  SET bio = %s WHERE user_id = %s',
-        (data['bio'], user_id)
-    )
-    db.get_db().commit()
-    return jsonify({'message': 'User updated successfully'}), 200
-
-# GET /stocks
-# [Alex-1]
-@experiencedTrader.route('/stocks-in-portfolio/<portoflio_id>', methods=['GET'])
-def get_all_stocks_in_portfolio(portoflio_id):
-    current_app.logger.info('GET /personalPortfolio{portfolio_id} route')
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM personalPortfolio WHERE portfolio_id = %s', (portoflio_id,))
-    theData = cursor.fetchall()
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-# GET /myportfolios
+# GET /portfolios/{user_id}
+# [Alex ADDED]
+# Displays their specific portfolios data
 @experiencedTrader.route('/myportfolios/<int:user_id>', methods=['GET'])
 def get_my_portfolios(user_id):
     current_app.logger.info(f'GET /personalPortfolio/{user_id} route')
@@ -111,6 +85,9 @@ def get_my_portfolios(user_id):
     the_response.mimetype = 'application/json'
     return the_response
 
+# GET /portfolios/{user_id}
+# [Alex ADDED]
+# Displays their specific stocks within their portfolio
 @experiencedTrader.route('/portfolios_stock/<int:user_id>', methods=['GET'])
 def get_portfolios_userID(user_id):
     current_app.logger.info(f'GET /personalPortfolio/{user_id} route')
@@ -123,44 +100,21 @@ def get_portfolios_userID(user_id):
     return the_response
 
 # POST /users
-@experiencedTrader.route('/follow', methods=['POST'])
-def follow_user():
-    data = request.json
-    follower_id = data.get('follower_id') 
-    following_id = data.get('following_id')
-    current_app.logger.info(f'POST /follow route - Follower ID: {follower_id}, Following ID: {following_id}')
+# [Alex-ADDED]
+# This functions allows the user to follow other verified people
+@experiencedTrader.route('/follow/<int:user_id>/<int:following_id>', methods=['POST'])
+def follow_user(user_id, following_id):
+    current_app.logger.info(f'POST /follow/{user_id}/{following_id} route')
     cursor = db.get_db().cursor()
-    cursor.execute('''
-        INSERT INTO follows (follower_id, following_id, timestamp, count, user_id)
-        VALUES (%s, %s, CURRENT_TIMESTAMP, %s, %s)
-    ''', (follower_id, following_id, follower_id))
-    db.get_db().commit()
-    the_response = make_response({'message': 'User followed successfully'})
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    cursor.execute('INSERT INTO follows (follower_id, following_id) VALUES (%s, %s)', (user_id, following_id))
+    response = make_response({'message': f'User {user_id} followed {following_id} successfully'})
+    response.status_code = 200
+    response.mimetype = 'application/json'
+    return response
 
-
-# DELETE /users
-
-@experiencedTrader.route('/unfollow', methods=['DELETE'])
-def unfollow_user():
-    data = request.json
-    follower_id = data.get('follower_id') 
-    following_id = data.get('following_id')  
-    current_app.logger.info(f'DELETE /unfollow route - Follower ID: {follower_id}, Following ID: {following_id}')
-    cursor = db.get_db().cursor()
-    cursor.execute('''
-        DELETE FROM follows
-        WHERE follower_id = %s AND following_id = %s
-    ''', (follower_id, following_id))
-    db.get_db().commit()
-    the_response = make_response({'message': 'User unfollowed successfully'})
-    the_response.status_code = 200 
-    the_response.mimetype = 'application/json'
-    return the_response
-
-
+# GET /public_profile/{user_id}
+# [Alex_1]
+# This function allows the verified user to see public profiles of other users by user_id
 @experiencedTrader.route('/public_profile/<user_id>', methods=['GET'])
 def get_public_profile(user_id):
     current_app.logger.info('GET /public_profile{user_id} route')
@@ -172,6 +126,9 @@ def get_public_profile(user_id):
     the_response.mimetype = 'application/json'
     return the_response
 
+# PUT /updateProfile
+# [Alex-4]
+# This function updates the users profile biography.
 @experiencedTrader.route('/update-bio/<int:user_id>/<update_bio>', methods=['PUT'])
 def update_bio(user_id, update_bio):
     current_app.logger.info(f'PUT /update-bio/{user_id}/{update_bio} route')
@@ -184,13 +141,16 @@ def update_bio(user_id, update_bio):
         )
         db.get_db().commit()
 
-        # Return a success message with status code 200
+        
         return jsonify({'message': 'Bio updated successfully'}), 200
 
     except Exception as e:
         current_app.logger.error(f"Error updating bio for user {user_id}: {str(e)}")
         return jsonify({'error': 'Failed to update user bio'}), 500
 
+# PUT /updateProfile
+# [Alex-4]
+# This function updates the users profile username
 @experiencedTrader.route('/update-username/<int:user_id>/<update_username>', methods=['PUT'])
 def update_username(user_id, update_username):
     current_app.logger.info(f'PUT /update-bio/{user_id}/{update_username} route')
@@ -210,7 +170,9 @@ def update_username(user_id, update_username):
         current_app.logger.error(f"Error updating bio for user {user_id}: {str(e)}")
         return jsonify({'error': 'Failed to update user bio'}), 500
 
-
+# PUT /updateProfile
+# [Alex-4]
+# This function updates the users profile picture
 @experiencedTrader.route('/update-photo/<int:user_id>/<update_photo>', methods=['PUT'])
 def update_photo(user_id, update_photo):
     current_app.logger.info(f'PUT /update-bio/{user_id}/{update_photo} route')
@@ -230,8 +192,9 @@ def update_photo(user_id, update_photo):
         current_app.logger.error(f"Error updating bio for user {user_id}: {str(e)}")
         return jsonify({'error': 'Failed to update user bio'}), 500
     
-
-
+# Post /notifications
+# [Alex-1]
+# This functions allows the verified user to create notifications
 @experiencedTrader.route('/create-notifications/<text>', methods=['POST'])
 def create_notifications(text):
     current_app.logger.info(f'POST /create-notifications{text} route')
@@ -249,6 +212,9 @@ def create_notifications(text):
         current_app.logger.error(f"Error creating notification: {str(e)}")
         return jsonify({'error': 'Failed to create notification'}), 500
     
+# Get /notifications/<user_id>
+# [Alex ADDED]
+# This function returns all of their previous written notifcations.
 @experiencedTrader.route('/get_notifications/<user_id>', methods=['GET'])
 def get_all_written_notifications(user_id):
     current_app.logger.info('GET /notifications{user_id} route')
@@ -262,6 +228,9 @@ def get_all_written_notifications(user_id):
     the_response.mimetype = 'application/json'
     return the_response
 
+# DELETE /notifications/{notif_id}
+# [Alex-1]
+# This function allows the user to delete a notication they created by notification_id
 @experiencedTrader.route('/delete_notifications/<notif_id>', methods=['DELETE'])
 def delete_written_notifications(notif_id):
     current_app.logger.info(f'DELETE /notifications/{notif_id} route')
@@ -277,6 +246,9 @@ def delete_written_notifications(notif_id):
         current_app.logger.error(f"Error deleting notification: {str(e)}")
         return jsonify({'error': 'Failed to delete notification'}), 500
 
+# PUT /notifications/{notif_id}
+# [Alex-1]
+# This function allows the user to update a notication they created by notification_id
 @experiencedTrader.route('/update_notifications/<notif_id>/<message>', methods=['PUT'])
 def update_written_notifications(notif_id, message):
     current_app.logger.info(f'PUT /update_notifications/{notif_id} with message: {message}')
@@ -291,7 +263,10 @@ def update_written_notifications(notif_id, message):
     except Exception as e:
         current_app.logger.error(f"Error updating notification: {str(e)}")
         return jsonify({'error': 'Failed to update notification'}), 500
-    
+
+# GET /recStocks
+# [Alex-ADDED]
+# Returns top 5 rec. stocks that the stock has the lowest beta of all stocks.
 @experiencedTrader.route('/recstocks', methods=['GET'])
 def get_all_stocks():
     current_app.logger.info('GET /stocks route')
